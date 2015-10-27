96c49ceb058630db8d1ecae3781babf311284d4d
sbkwgh <sbkwgh@gmail.com>
2015-10-27 19:08:36 +0000

    Add config route to routes file

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 var post = require('./routes/api/post.js');
 var user = require('./routes/api/user.js');
+var config = require('./routes/api/config.js');
 
 var blog = require('./routes/blog.js');
 var index = require('./routes/index.js');
@@ -12,4 +13,5 @@ module.exports = function(app) {
 	app.use('/admin', admin);
 	app.use('/api/post', post);
 	app.use('/api/user', user);
+	app.use('/api/config', config);
 };
