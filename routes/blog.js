fe9a4dae20f1b3218965243d7ef829571434e65b
sbkwgh <sbkwgh@gmail.com>
2015-10-27 19:04:01 +0000

    Update blog routes and static files to make use of dynamic (user selected) css

--- routes/blog.js
+++ routes/blog.js
@@ -1,36 +1,37 @@
 var router = require('express').Router();
 var Post = require('../models/post.js');
+var Config = require('../models/config.js');
+
+function renderBlogCb(err, posts, req, res) {
+	if(err) {
+		console.log(err);
+		res.render('blog.html', {posts: []})
+	} else {
+		Config.getOrCreateConfig(function(err, config) {
+			if(err) {
+				res.render('blog.html', {posts: []})
+			} else {
+				res.render('blog.html', {config:config, posts: posts});
+			}
+		})
+	}
+}
 
 router.get('/posts', function(req, res) {
 	Post.find({published: true}, function(err, posts) {
-		if(err) {
-			console.log(err);
-			res.render('blog.html', {posts: []})
-		} else {
-			res.render('blog.html', {posts: posts});
-		}
+		renderBlogCb(err, posts, req, res)
 	});
 })
 
 router.get('/posts/:_id', function(req, res) {
 	Post.find({published: true, _id: req.params._id}, function(err, posts) {
-		if(err) {
-			console.log(err);
-			res.render('blog.html', {posts: []})
-		} else {
-			res.render('blog.html', {posts: posts});
-		}
+		renderBlogCb(err, posts, req, res)
 	});
 })
 
 router.get('/tag/:tag', function(req, res) {
 	Post.find({published: true, tags: req.params.tag}, function(err, posts) {
-		if(err) {
-			console.log(err);
-			res.render('blog.html', {posts: []})
-		} else {
-			res.render('blog.html', {posts: posts});
-		}
+		renderBlogCb(err, posts, req, res)
 	});
 })
 
