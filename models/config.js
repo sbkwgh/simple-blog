f088b6b17c525096104613809a3fd7a55c47b29e
sbkwgh <sbkwgh@gmail.com>
2015-10-27 19:12:14 +0000

    Add config model and route to get/set/update the custom settings for the blog

---
+++ models/config.js
@@ -0,0 +1,73 @@
+var mongoose = require('mongoose');
+
+var configSchema = mongoose.Schema({
+	appearance: {
+		font: {
+			default: String,
+			postTitle: String,
+			postBody: String,
+			blogTitle: String
+		},
+		colour: {
+			link: {
+				default: String,
+				visited: String,
+				active: String,
+				hover: String
+			},
+			blogTitle: String,
+			topBar: String,
+			postTitle: String,
+			postBody: String
+		}
+	},
+	general: {
+			blog: {
+				title: String,
+				description: String
+			}
+	}
+});
+
+configSchema.statics.getOrCreateConfig = function(cb) {
+	var self = this;
+	this.findOne({}, function(err, config) {
+		if(config === null) {
+			var newConfig = new self();
+			newConfig.appearance = {
+				font: {
+					default: 'Arial',
+					postTitle: 'Arial',
+					postBody: 'Arial',
+					blogTitle: 'Arial'
+				},
+				colour: {
+					link: {
+						default: '#5F7F9C',
+						visited: '#477C95',
+						active: '#8EB7CB',
+						hover: '#7EA7DB'
+					},
+					blogTitle: '#6C7172',
+					topBar: '#78AEF3',
+					postTitle: '#000000',
+					postBody: '#000000'
+				}
+			};
+			newConfig.general = {
+				blog: {
+					title: 'Blog title',
+					description: 'A description of your blog. Change this in settings → general'
+				}
+			}
+
+			newConfig.save(cb)
+		} else {
+			cb(null, config);
+		}
+	});
+};
+
+var Config = mongoose.model('Config', configSchema);
+
+module.exports = Config;
---
+++ routes/api/config.js
@@ -0,0 +1,105 @@
+var router = require('express').Router();
+var Config = require('../../models/config.js');
+
+router.all('*', function(req, res, next) {
+	if(!req.signedCookies.loggedInUser) {
+		res.json({error: 'Request not authorised'})
+	} else {
+		next();
+	}
+})
+
+router.get('/', function(req, res) {
+	Config.getOrCreateConfig(function(err, config) {
+		if(err) {
+			res.json({error: 'unknown error'})
+		} else {
+			res.json(config);
+		}
+	})
+})
+
+router.get('/css.css', function(req, res) {
+	Config.getOrCreateConfig(function(err, config) {
+		if(err) {
+			console.log(err)
+			res.send('unknown error')
+		} else {
+			var css = 
+				"html, body {" + 
+				"	font-family: '" + config.appearance.font.default + "', 'sans-serif';" +
+				"}" +
+				".post-body {" +
+				"	font-family: '" + config.appearance.font.postBody + "', 'sans-serif';" +
+				"	color: " + config.appearance.colour.postBody + ";" +
+				"}" +
+				".post-title {" +
+				"	font-family: '" + config.appearance.font.postTitle + "', 'sans-serif';" +
+				"	color: " + config.appearance.colour.postTitle + ";" +
+				"}" +
+				"a:hover {" +
+				"	color: " + config.appearance.colour.link.hover + ";" +
+				"}" +
+				"a:active {" +
+				"	color: " + config.appearance.colour.link.active + ";" +
+				"}" +
+				"a: {" +
+				"	color: " + config.appearance.colour.link.default + ";" +
+				"}" +
+				"a:visited {" +
+				"	color: " + config.appearance.colour.link.visited + ";" +
+				"}" +
+				"#color-bar {" +
+				"	background-color: " + config.appearance.colour.topBar + ";" +
+				"}" + 
+				"header a {" +
+				"	font-family: '" + config.appearance.font.blogTitle + "', 'sans-serif';" +
+				"	color: " + config.appearance.colour.blogTitle + ";" +
+				"}";
+
+			res.header('Content-type', 'text/css')
+			res.send(css);
+		}
+	})
+});
+
+router.get('/js.js', function(req, res) {
+	Config.getOrCreateConfig(function(err, config) {
+		if(err) {
+			console.log(err)
+			res.send('unknown error')
+		} else {
+			var fontFamilies = [
+				config.appearance.font.default,
+				config.appearance.font.postBody,
+				config.appearance.font.postTitle,
+				config.appearance.font.blogTitle
+			];
+			var js =
+				'WebFont.load({' +
+				'	google: {' +
+				'		families:' + '["' + fontFamilies.join('","') + '"]' +
+				'	}' +
+				'});'
+
+			res.header('Content-type', 'text/javascript');
+			res.send(js)
+		}
+	});
+});
+
+router.put('/', function(req, res) {
+	var configObject = JSON.parse(req.body.json)
+
+	Config.findOneAndUpdate({}, configObject, {upsert: true}, function(err, updatedConfig) {
+		if(err) {
+			console.log(err)
+			res.json({error: 'unknown error'});
+			return;
+		}
+
+		res.json(updatedConfig);
+	});
+});
+
+module.exports = router;
