e02c4945b9028baf017740a933261a615e10a8eb
sbkwgh <sbkwgh@gmail.com>
2015-10-30 18:36:13 +0000

    Convert comments to markdown on saving, add a getNumberOfComments static method

--- models/comment.js
+++ models/comment.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var markdown = require( "markdown" ).markdown;
 
 var commentSchema = mongoose.Schema({
 	postId: mongoose.Schema.ObjectId,
@@ -6,7 +7,30 @@ var commentSchema = mongoose.Schema({
 	parentCommentName: String,
 	name: String,
 	body: String,
+	authorComment: Boolean,
 	date: {type: Date, default: Date.now}
+});
+
+commentSchema.statics.getNumberOfComments = function(cb) {
+	var commentNumbers = {};
+	this.find({}, function(err, comments) {
+		if(err) cb(err);
+
+		comments.forEach(function(comment) {
+			if(!commentNumbers[comment.postId]) {
+				commentNumbers[comment.postId] = 1;
+			} else {
+				commentNumbers[comment.postId] += 1;
+			}
+		})
+
+		cb(null, commentNumbers);
+	})
+}
+
+commentSchema.pre('save', function(next) {
+	this.body = markdown.toHTML(this.body);
+	next();
 });
 
 var Comment = mongoose.model('Comment', commentSchema);
